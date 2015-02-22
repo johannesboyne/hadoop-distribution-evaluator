e682061f8e68d3f9eb4288e2728125c09944d991
Johannes Boyne <johannes@boyne.de>
2015-02-22 16:26:19 +0100

    inital commit

---
+++ public/app.js
@@ -0,0 +1,97 @@
+angular.module('DBISEvaluation', [])
+  .controller('FormController', ['$scope', '$http', function($scope, $http) {
+    $scope.count = 0
+    $scope.data = null
+    $http.get('matrix_data.json').then(function (res) {
+      $scope.data = res.data
+    })
+    $scope.percents = function() {
+      $scope.count = 0;
+      angular.forEach($scope.questions, function(q) {
+        $scope.count += q.val;
+        if (q.subs) {
+          q.subcount = 0
+          angular.forEach(q.subs, function (sq) {
+            q.subcount += sq.val;
+          })
+        }
+      });
+    }
+    $scope.percents()
+
+    $scope.sum_all_cats = function () {
+      if ($scope.data === null) return
+      return $scope.data.categories
+      .map(function (c) {
+        return $scope.calc_category(c)
+      })
+      .reduce(function (a,b) { return a+b })
+    }
+
+    $scope.calc_category = function (val) {
+      if (val.sub_categories.length <= 1)
+        return val.cat_weight
+      return Math.round((val.sub_categories.map(function (sc) { return sc.cat_weight*val.cat_weight }).reduce(function (prev, cur) { return prev + cur; }))/100)
+    }
+
+    $scope.calc_sub_category = function (nindex, sc, val) {
+      return val.sub_categories
+      .map(function (sc) { return sc.distribution_values
+           .map(function (dv) { return dv.points*sc.cat_weight*val.cat_weight }) })
+           .reduceRight(function (p,c) { return p.map(function (_p, i) { return _p + c[i] }) })[nindex]/100
+    }
+
+    $scope.calc_weight = function (sub, val) {
+      return Math.round(sub.cat_weight*val.cat_weight/100)
+    }
+
+    $scope.sum_all_subs = function (val) {
+      return val.sub_categories
+      .map(function (v) { return v.cat_weight })
+      .reduce(function (a,b) {
+        return a+b
+      })
+    }
+
+    $scope.check_subs = function (q) {
+      return $scope.sum_all_subs(q) != 100
+    }
+
+    $scope.cat_score = function (d,j) {
+      return $scope.data.categories
+      .map(function (mc) { 
+        return mc.sub_categories
+        .map(function (sc) { 
+          return sc.distribution_values
+          .map(function (dv) { 
+            return dv.points*(sc.cat_weight*mc.cat_weight)
+          }) 
+        })
+        .reduceRight(function (p,c) { 
+          return p.map(function (_p, i) { return _p + c[i] }) 
+        }) 
+      })
+      .reduceRight(function (p,c) { 
+        return p.map(function (_p, i) { return _p + c[i] }) 
+      })[j]/10000
+    }
+
+    $scope.swot_score = function (d,j) {
+      var count = 0
+      $scope.data.swot_questions.map(function (q) {
+        if (q.value)
+          count += q.distribution_values[j].points
+        return q
+      })
+      return count
+    }
+
+    $scope.submit = function () {
+      $http.post('submit', $scope.data).then(function (res) {
+        if (res.data.info == 'success') {
+          return alert('Successfully saved, thanks a lot!')
+        }
+        alert('Sorry, the request terminated...')
+      })
+    }
+}])
---
+++ server.js
@@ -0,0 +1,55 @@
+var fs = require('fs')
+var express = require('express')
+var jade = require('jade')
+var stylus = require('stylus')
+var app = express()
+
+app.set('view engine', 'jade');
+
+app.use(stylus.middleware({
+  src: __dirname + '/resources',
+  dest: __dirname + '/public',
+  debug: true,
+  force: true
+}))
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html', 'css', 'js', 'png', 'jpg', 'json'],
+  index: false,
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())
+  }
+}
+app.use(express.static('public', options))
+app.use(express.static('bower_components', options))
+app.use(express.static('predefault_data', options))
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'DBIS | Hadoop Distribution Evaluator', message: 'Hello there!', data: require('./predefault_data/matrix.json')})
+})
+
+app.get('/matrix_data.json', function (req, res) {
+  res.json(require('./predefault_data/matrix.json'))
+})
+
+app.post('/submit', function (req, res) {
+  try {
+    req.pipe(fs.createWriteStream('./'+Date().toString().replace(/\D/g, '')+'.json'))
+    req.on('end', function () {
+      res.json({info: "success"})
+    })
+  } catch (e) {
+    res.json({info: "error"})
+  }
+})
+
+var server = app.listen(3001, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
